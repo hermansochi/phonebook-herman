5d83dde851fa4c3553e6d65b020621d6dabf8d05
Herman Alexandrov <hermansochi@ya.ru>
2023-01-12 12:34:38 +0300

    Herman: Front: Add tailwindcss into Vue frontend.

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-green-500 text-2xl">Clone</h1>
 </template>
 
 <style scoped>
